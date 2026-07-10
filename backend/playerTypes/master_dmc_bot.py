11094207d71346259c99b8672c10e06dd4a4d033
yeetinator <dmclz.boone@gmail.com>
2026-07-10 02:08:19 +0200

    Made opponent hands vector relative

--- backend/playerTypes/master_dmc_bot.py
+++ backend/playerTypes/master_dmc_bot.py
@@ -29,13 +29,13 @@ def vectorize_master_state(state, env: President, num_players=4):
     MAX_PLAYERS = 7
     opp_hands_matrix = np.full((MAX_PLAYERS - 1, 13), -1.0, dtype=np.float32)
 
-    slot = 0
-    for p_id in range(num_players):
-        if p_id != my_id:
+    for i in range(1, MAX_PLAYERS):
+        slot = i - 1
+        if i < num_players:
+            opp_id = (my_id + i) % num_players
             opp_hands_matrix[slot, :] = 0.0
-            for card in env.hands[p_id]:
+            for card in env.hands[opp_id]:
                 opp_hands_matrix[slot, card - 3] += 1.0 / 4.0
-            slot += 1
 
     opp_hands_vec = opp_hands_matrix.flatten()  # 78
     return np.concatenate([student_vec, opp_hands_vec])
