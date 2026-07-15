960d26b71926ce34fcf768a4a768d1b2bc73b949
yeetinator <dmclz.boone@gmail.com>
2026-07-15 19:53:45 +0200

    Added . for relative imports

--- backend/playerTypes/human.py
+++ backend/playerTypes/human.py
@@ -1,4 +1,4 @@
-from player import Player
+from .player import Player
 from game import President, get_val_input
 from collections import Counter
 
--- backend/playerTypes/random_bot.py
+++ backend/playerTypes/random_bot.py
@@ -1,5 +1,5 @@
 import random
-from player import Player
+from .player import Player
 
 
 class PresidentRandomBot(Player):
