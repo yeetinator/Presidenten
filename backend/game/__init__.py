fe9a29d091842f966a4b6384d76ce2be26de4743
yeetinator <dmclz.boone@gmail.com>
2026-07-15 20:14:02 +0200

    removed circular import

--- backend/game/__init__.py
+++ backend/game/__init__.py
@@ -1,24 +1,5 @@
 from .cli_utils import get_settings, get_val_input
 from .engine import President
-from .tournament import (
-	create_players,
-	game_parallelism,
-	play_president_game,
-	print_scores,
-	search_parallelism,
-	update_final_scores,
-)
 from .types import PlayerType
 
-__all__ = [
-	"PlayerType",
-	"President",
-	"get_settings",
-	"get_val_input",
-	"create_players",
-	"game_parallelism",
-	"play_president_game",
-	"print_scores",
-	"search_parallelism",
-	"update_final_scores",
-]
+__all__ = ["PlayerType", "President", "get_settings", "get_val_input"]
